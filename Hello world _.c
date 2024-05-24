5be551291de55798c96460654962ce870d9a4b39
Anoskie <anoskie551@gmail.com>
2024-05-24 07:29:33 +0300

    Add files via upload

---
+++ Hello world _.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("iam anoskie\n");
+    printf("software engineer \n");
+    printf("zetech University \n");
+    return 0;
+}      
---
+++ Sum.c
@@ -0,0 +1,17 @@
+//program to add two numbers 
+/*
+Program to add two numbers 
+Author: Arnold 
+Date:16/05/2024
+*/
+#include<stdio.h>//scanf, printf
+int main() {
+int a = 10;
+int b = 20;
+int sum;
+sum = a + b;
+printf("The sum of a and b is %d",sum);
+
+return 0;
+}
+
