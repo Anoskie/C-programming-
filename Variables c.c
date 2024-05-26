de17cda47fceb8a8965b670d847b4ff7ff8dca5d
Anoskie <anoskie551@gmail.com>
2024-05-26 18:44:15 +0300

    Add files via upload

---
+++ Variables c.c
@@ -0,0 +1,18 @@
+//variables and data types in C
+#include<stdio.h> //scanf(), printf()
+
+int a=20;//global variable
+
+int main (){
+  //int a=11; //local variable 
+  printf("The value of a is %d \n",a);
+  area();
+
+return 0;
+}
+
+int area(){  
+//int a=12;
+printf("The value of a is %d \n",a);
+return 0;
+}
